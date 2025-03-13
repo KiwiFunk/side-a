dc1366bc9b6eba941d62a08b0e09b2f36c1e381a
Alex <alex_p97@live.com>
2025-03-13 22:02:20 +0000

    Created basic tracklist component

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,16 @@ import './styles/global.css';
 
 import Header from './components/Header';
 import MainScene from './components/MainScene';
+import Tracklist from './components/Tracklist';
 
 function App() {
     return (
         <div className="App">
             <Header />
             <MainScene />
+            <Tracklist />
 
-            <div style={{ padding: '50px', background: '#f0f0f0', textAlign: 'center' }}>
+            <div style={{ padding: '50px', height: '100vh', background: '#f0f0f0', textAlign: 'center' }}>
                 <h2>Welcome to the Mixtape Builder</h2>
                 <p>Scroll down to see the magic happen!</p>
             </div>
--- client/src/components/Tracklist.js
+++ client/src/components/Tracklist.js
@@ -1,1 +1,32 @@
 //Display the tracklist for the current selected mixtape after the 3d animation has finished.
+import React from 'react';
+
+function Tracklist() {
+    return (
+        <section className='tracklist'>
+            <h2>Username's Playlist:</h2>
+            <div className='tracklist-column'>
+                <h3>Side <span className='side-circle'>A</span></h3>
+                <ol>
+                    <li><span className='track-name'>Next Semester</span><span className='artist-name'>Twenty One Pilots</span></li>
+                    <li><span className='track-name'>Cheerleader</span><span className='artist-name'>Porter Robinson</span></li>
+                    <li><span className='track-name'>Favourite</span><span className='artist-name'>Fontaines D.C.</span></li>
+                    <li><span className='track-name'>Tomorrow Is Closed</span><span className='artist-name'>Nothing But Thieves</span></li>
+                    <li><span className='track-name'>Stereo Play</span><span className='artist-name'>James Ivy</span></li>
+                </ol>
+            </div>
+            <div className='tracklist-column'>
+                <h3>Side <span className='side-circle'>B</span></h3>
+                <ol>
+                    <li><span className='track-name'>Hardest To Love</span><span className='artist-name'>The Weekend</span></li>
+                    <li><span className='track-name'>Heaven Is Calling</span><span className='artist-name'>Jai Wolf</span></li>
+                    <li><span className='track-name'>Marigold</span><span className='artist-name'>Frost Children</span></li>
+                    <li><span className='track-name'>Chinatown</span><span className='artist-name'>Bleachers ft. Bruce Springsteen</span></li>
+                    <li><span className='track-name'>Unbound</span><span className='artist-name'>Shallou</span></li>
+                </ol>
+            </div>
+        </section>
+    );
+}
+
+export default Tracklist;
